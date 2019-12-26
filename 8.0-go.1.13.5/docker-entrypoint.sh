d93fd21f0c3475998fb0c12f167af2b9c8b0e9d1
xltxlm <xltxlm@qq.com>
2019-12-26 15:47:21 +0800

    go版本

--- 8.0-go.1.13.5/docker-entrypoint.sh
+++ 8.0-go.1.13.5/docker-entrypoint.sh
@@ -8,6 +8,10 @@ MYSQL_DATABASE="demo"
 MYSQL_USER="demo"
 MYSQL_PASSWORD="demo"
 
+#输出错误和成功信息的函数,带底色
+function error(){ echo  -e "\033[41;36m $1 \033[0m";}
+function ok(){ echo  -e "\033[42;30m $1 \033[0m";}
+
 # logging functions
 mysql_log() {
 	local type="$1"; shift
@@ -366,6 +370,7 @@ _main() {
 			echo
 		fi
 	fi
+	ok "$@"
 	exec "$@"
 }
 
