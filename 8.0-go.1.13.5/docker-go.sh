da6466169a0d868685b535151e7391eebdfdef48
xltxlm <xltxlm@qq.com>
2019-12-26 16:55:17 +0800

    触发hub.docker编译

---
+++ 8.0-go.1.13.5/docker-go.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#初始化root账户
+export MYSQL_ROOT_PASSWORD="123456"
+export MYSQL_DATABASE="demo"
+export MYSQL_USER="demo"
+export MYSQL_PASSWORD="demo"
+
+#输出错误和成功信息的函数,带底色
+function error(){ echo  -e "\033[41;36m $1 \033[0m";}
+function ok(){ echo  -e "\033[42;30m $1 \033[0m";}
+
+#启动mysql服务
+docker-entrypoint.sh mysqld &
+
+#启动go语言本身服务
+/bin/bash /root/CMD.sh
