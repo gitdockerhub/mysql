b0f6ebe8288245d2bf514ab3b9151af112a1592d
xltxlm <xltxlm@qq.com>
2019-12-27 10:08:06 +0800

    触发hub.docker编译

--- 8.0-go.1.13.5/docker-go.sh
+++ 8.0-go.1.13.5/docker-go.sh
@@ -12,5 +12,19 @@ function ok(){ echo  -e "\033[42;30m $1 \033[0m";}
 #启动mysql服务
 docker-entrypoint.sh mysqld &
 
+for sitenu in $(seq 1 100)
+do
+  #观察mysql的端口是否已经起来了
+  mysql_up=$(netstat -tnlpu | grep 3306 | wc-l)
+  if [ "$mysql_up" =  "1" ]; then
+    ok "mysql 服务启动成功!"
+    break ;
+  fi
+  sleep 1
+done
+if [ "$mysql_up" !=  "1" ]; then
+    error "mysql 服务启动失败[n]"
+    exit 1
+fi
 #启动go语言本身服务
 source /root/CMD.sh
